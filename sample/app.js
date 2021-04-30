41283431fe4be8819e6bb97c7a4eb0147b519a6b
takenoko <takenoko.tech@gmail.com>
2021-04-30 14:33:00 +0900

    fix sample.

--- sample/app.js
+++ sample/app.js
@@ -11,11 +11,23 @@ Firearm.setup({
     }]
 })
 
-const img = document.getElementById('sample_image');
-const result = Firearm.predict("mobilenet", img)
+function predict() {
+    const img = document.getElementById('sample_image');
+    const result = Firearm.predict("mobilenet", img)
+    result.then(it => {
+        console.log(it[0])
+        console.log(it[1])
+        console.log(it[2])
+        document.getElementById("predict_result").innerHTML=it[0].label;
+    })
+}
 
-result.then(it => {
-    console.log(it[0])
-    console.log(it[1])
-    console.log(it[2])
-})
+function changedImage(obj) {
+    console.log("changedImage")
+    var fileReader = new FileReader()
+    fileReader.onload = () => document.getElementById('sample_image').src = fileReader.result
+    fileReader.readAsDataURL(obj.files[0])
+    predict()
+}
+
+predict()
